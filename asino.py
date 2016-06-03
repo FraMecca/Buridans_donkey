673b3d1c52d3cf85507460420a47eb8c6d4b9277
Your Name <you@example.com>
2016-06-02 22:30:37 -0400

    redefined asino

--- asino.py
+++ asino.py
@@ -1,14 +1,48 @@
-import random, sys
+#!/usr/bin/python3
+import random
 
-if __name__ == '__main__':
-    args = list()
-    if not sys.stdin.isatty():
-        for line in sys.stdin:
+def parse_args ():
+    import argparse
+    parser = argparse.ArgumentParser ()
+    parser.add_argument ('-n', '--no-numbers', action = 'store_true', default = False,
+                         help = 'do not display line numbers')
+    parser.add_argument ('choices', type = str, nargs= '*', default = [],
+                         help = 'where the various choices belong')
+    parser.add_argument ('-l', '--limit', type = int, default = False,
+                         help = 'display n lines')
+    return parser.parse_args ()
+
+_cnt = -1
+def enum ():
+    global _cnt
+    _cnt += 1
+    return (''.join ([str (_cnt), '. ']))
+def null_string ():
+    return ''
+
+def main ():
+    from sys import stdin
+    args = parse_args ()
+
+    if args.limit:
+        limit = args.limit - 2
+    if args.no_numbers:
+        fst = null_string
+    else:
+        fst = enum
+
+    if not stdin.isatty():
+        for line in stdin:
             if line[-1] is '\n':
                 line = line[:-1]
-            args.append(line)
-    else:
-        args = sys.argv[1:]
-    argRange = len(args)
-    for i in range(argRange):
-        print(str(i+1) + '.', args.pop(random.randrange(0, len(args))))    
+            args.choices.append(line)
+
+    argRange = len(args.choices)
+    for i in range (argRange):
+        print (''.join ([fst (), args.choices.pop(random.randrange(0, len(args.choices)))]))
+        if args.limit and i > limit:
+            exit ()
+
+
+if __name__ == '__main__':
+    main ()
